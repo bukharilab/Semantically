59acd026e81b4fa003a5887f7173262d5a3a1219
Steve Mbouadeu <steve.mbouadeu19@stjohns.edu>
2020-11-17 11:57:52 -0500

    Building user interface.

--- webapp/scripts.js
+++ webapp/scripts.js
@@ -3,52 +3,124 @@ $('#annotate').click(() => showAnnotations());
 
 function showAnnotations() {
   //annotatingEnabled = true;
-  show('loader');
+  showLoader();
   getRecommenderAnnotations(getText(), (data) => {
     console.log(data);
     showDownloadButton();
-    hide('loader');
+    hideLoader();
   });
 }
 
-function highlight(start, end) {
-  console.log('started');
+function getLineEnds() {
+  const text = getText();
+
+  let prevIdx = 0;
+  let currIdx = 0;
   const editor = document.querySelector("trix-editor").editor;
-  if ($('#highlight').length === 0) {
-    console.log(1);
-    let elem = $('<span></span>');
-    elem.id = "highlight";
 
-    const startPos = editor.getClientRectAtPosition(start);
-    const endPos = editor.getClientRectAtPosition(end);
+  return Array.from(Array(text.length).keys()).reduce((lineEnds, idx) => {
+    let curr = editor.getClientRectAtPosition(idx);
+    if (idx === text.length-1) {
+      lineEnds[curr.top] = idx;
+      return lineEnds;
+    } else if (idx === 0) return lineEnds;
 
-    let width = endPos.left - startPos.left;
+    let prev = editor.getClientRectAtPosition(idx-1);
+    if (prev.top !== curr.top) {
+      lineEnds[prev.top] = idx-1;
+    }
+    return lineEnds;
+  }, {});
+}
 
-    elem.css({
-      width: width + 'px',
-      height: '5px',
-      borderRadius: '5px',
-      backgroundColor: '#6200ee',
-      position: 'absolute',
-      left: startPos.left + 'px',
-      top: (startPos.top + 22) + 'px'
-    });
-    $('body').append(elem);
-    console.log('posted');
-  } else {
-    console.log(2);
-    let elem = $('#highlight');
-    const startPos = editor.getClientRectAtPosition(start);
-    const endPos = editor.getClientRectAtPosition(end);
+function multiHighlight(start, end, startPos, endPos) {
+  const editor = document.querySelector("trix-editor").editor;
+  const lines = (endPos.top - startPos.top) / 28;
+  const lineEnds = getLineEnds();
 
-    let width = endPos.left - startPos.left;
+  // First line
+  let firstLineEnd = lineEnds[startPos.top];
+  mark(start, firstLineEnd, startPos, editor.getClientRectAtPosition(firstLineEnd));
 
-    elem.css({
-      width: width + 'px',
-      left: startPos.left + 'px',
-      top: (start.top + 26) + 'px'
-    });
+  // Middle lines
+  let lastLineEnd = firstLineEnd;
+  for (let i = 0; i < lines-1; i++) {
+    let newStartPos = editor.getClientRectAtPosition(lastLineEnd+1);
+    let newEnd = lineEnds[newStartPos.top];
+    let newEndPos = editor.getClientRectAtPosition(newEnd);
+    mark(lastLineEnd+1, newEnd, newStartPos, newEndPos);
+    lastLineEnd = newEnd;
   }
+
+  // Last line
+  let newStartPos = editor.getClientRectAtPosition(lastLineEnd+1);
+  mark(lastLineEnd+1, end, newStartPos, endPos);
+}
+
+function mark(start, end, startPos, endPos) {
+  let elem = $('<span class="highlight"></span>');
+
+  // console.log(startPos);
+  // console.log(endPos);
+  let width = endPos.left - startPos.left;
+  elem.css({
+    left: startPos.left + 'px',
+    top: (startPos.top + 22) + 'px',
+    with: '5px'
+  });
+
+  $('body').append(elem);
+  setTimeout(() => {
+    elem.css({
+      width: width + 'px'
+    });
+  }, 100);
+}
+
+function highlight(start, end) {
+  $('body').children('.highlight').remove();
+
+  const editor = document.querySelector("trix-editor").editor;
+  const startPos = editor.getClientRectAtPosition(start);
+  const endPos = editor.getClientRectAtPosition(end);
+
+  if (startPos.top !== endPos.top) {
+    multiHighlight(start, end, startPos, endPos);
+    return;
+  }
+  mark(start, end, startPos, endPos);
+  return;
+
+
+
+    // elem.hover(() => {
+    //
+    // }, () => {
+    //   elem.css({
+    //     width: '5px'
+    //   });
+    // });
+  // } else {
+  //   console.log(2);
+  //   elem = $('#highlight');
+  //
+  //
+  //   const startPos = editor.getClientRectAtPosition(start);
+  //   const endPos = editor.getClientRectAtPosition(end);
+  //
+  //   let width = endPos.left - startPos.left;
+  //   elem.css({
+  //     left: startPos.left + 'px',
+  //     top: (start.top + 26) + 'px'
+  //   });
+  //
+  //   $('body').append(elem);
+  //
+  //   elem.css({
+  //     width: width + 'px'
+  //   });
+  // }
+  // elem.addClass('active');
 }
 
 function showDownloadButton() {
@@ -69,6 +141,18 @@ function show(blockName) {
 function hide(blockName) {
   $('.sidebar-block').css('display', 'block');
   $('.' + blockName).css('display', 'none');
+}
+
+function showLoader() {
+  console.log('loader shown');
+  $('.loader-container').css('display', 'flex');
+  $('.sidebar-block').css('display', 'none');
+}
+
+function hideLoader() {
+  console.log('loader hidden');
+  $('.loader-container').css('display', 'none');
+  $('.sidebar-block').css('display', 'block');
 }
 
 function getText() {
