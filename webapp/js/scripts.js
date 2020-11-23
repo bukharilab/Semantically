82f162f7b244f43c83581b97a7f5224f94d0f6fe
Steve Mbouadeu <steve.mbouadeu19@stjohns.edu>
2020-11-23 18:50:53 -0500

    Add sidebar tabs

---
+++ webapp/js/scripts.js
@@ -0,0 +1,101 @@
+$('#annotate').click(() => showAnnotations());
+
+function showAnnotations() {
+  //annotatingEnabled = true;
+  showLoader();
+  getRecommenderAnnotations(getText(), (formattedRes) => {
+    console.log(formattedRes);
+    fillSidebarAccordion(formattedRes);
+    showDownloadButton();
+    hideLoader();
+  });
+}
+
+
+
+function showDownloadButton() {
+  $('.sidebar.bottom button#downloadAs').css('display', 'block');
+  $('.sidebar.branding-below').css('bottom', '127px');
+}
+
+function hideDownloadButton() {
+  $('.sidebar.bottom button#downloadAs').css('display', 'none');
+  $('.sidebar.branding-below').css('bottom', '80px');
+}
+
+function show(blockName) {
+  $('.sidebar-block').css('display', 'none');
+  $('.' + blockName).css('display', 'block');
+}
+
+function hide(blockName) {
+  $('.sidebar-block').css('display', 'block');
+  $('.' + blockName).css('display', 'none');
+}
+
+function showLoader() {
+  console.log('loader shown');
+  $('.loader-container').css('display', 'flex');
+  $('.sidebar-block').css('display', 'none');
+}
+
+function hideLoader() {
+  console.log('loader hidden');
+  $('.loader-container').css('display', 'none');
+  $('.sidebar-block').css('display', 'block');
+}
+
+
+
+// async function getAnnotations(enableHighlighting) {
+//   // if(enableHighlighting === true) {
+//   //   PropertiesService.getScriptProperties().setProperty('highlightingEnabled', true);
+//   // }
+//
+//   annotations = [];
+//
+//   // Getting Annotations
+//   var url = 'https://data.bioontology.org/annotator';
+//   var result = await $.ajax({
+//     url: url,
+//     data: {
+//       text: getText(),
+//       include: 'prefLabel,definition',
+//       display_context: 'false',
+//       apikey: '8b5b7825-538d-40e0-9e9e-5ab9274a9aeb'
+//     }
+//   });
+//   //console.log(result);
+//   for(i = 0; i < result.length; i++) {
+//     annotations.push(
+//       {
+//         id: result[i]['annotatedClass']['@id'],
+//         prefLabel: result[i]['annotatedClass']['prefLabel'].replace(/^\w/, c => c.toUpperCase()),
+//         definition: ((typeof result[i]['annotatedClass']['definition'] !== 'undefined') ?
+//           result[i]['annotatedClass']['definition'][0].split(/<p>|<\/p>/).join('') :
+//           ''),
+//         ontology: result[i]['annotatedClass']['links']['ontology'].replace(/http[s]?:\/\/data.bioontology.org\/ontologies\//g, ''),
+//         link: result[i]['annotatedClass']['links']['self'],
+//         annotations: result[i]['annotations']
+//       }
+//     );
+//   }
+//   //console.log(JSON.stringify(annotations));
+//   return JSON.stringify(annotations);
+// }
+
+
+
+
+function unhighlightAnnotation(a,b) {
+  var text = DocumentApp.getActiveDocument().getBody().editAsText();
+  text.setBackgroundColor(a , b , null);
+}
+
+
+function unhighlightAnnotations() {
+  PropertiesService.getScriptProperties().setProperty('highlightingEnabled', false);
+
+  var text = DocumentApp.getActiveDocument().getBody().editAsText();
+  text.setBackgroundColor(0, text.getText().length - 1, null);
+}
