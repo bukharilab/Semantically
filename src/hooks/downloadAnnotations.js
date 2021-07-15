04cae0d7a1018520f4a14f4552c704cc5f8da549
Steve Mbouadeu <mbouadeus@gmail.com>
2021-07-15 07:26:42 -0400

    fix highlighted annotations export bug

--- src/hooks/downloadAnnotations.js
+++ src/hooks/downloadAnnotations.js
@@ -1,6 +1,7 @@
 import getText from './getText';
 import getTermStr from './getTermString';
 import getDefinition from './getDefinition';
+import sortKeys from './sortKeys';
 
 const download = (filename, text) => {
   const element = document.createElement('a');
@@ -50,7 +51,10 @@ const saveAsHTML = async (annotations, annotationSelection, definitions, updateD
 
   let addedLength = 0;
   let text = getText();
-  for(const term in onlySelectedAnnotations) {
+  let annText = "";
+  let ended = 0;
+
+  for(const term of sortKeys(Object.keys(onlySelectedAnnotations))) {
     const annotation = onlySelectedAnnotations[term];
 
     schemaJson['code'].push({"@type": "MedicalCode", "name": getTermStr(term), "codingSystem": annotation.acronym, "link": annotation.annotatedClass['@id']});
@@ -58,12 +62,13 @@ const saveAsHTML = async (annotations, annotationSelection, definitions, updateD
       const url = annotation.annotatedClass.links.self;
       const def = url in definitions ? definitions[url] : await getDefinition(url);
       updateDefinitions({...definitions, [url]: def});
-      text = text.substring(0, annotation.from + addedLength - 1) + `<span class="tippy" title="${def}">${text.substring(annotation.from + addedLength - 1, annotation.to + addedLength)}</span>` + text.substring(annotation.to + addedLength);
-      addedLength += 36 + def.length;
+      annText += text.substring(ended, annotation.from - 1) + `<span class="tippy" title="${def}">${text.substring(annotation.from - 1, annotation.to)}</span>`;
+      ended = annotation.to;
     }
   }
+  annText += text.substring(ended);
 
-  download('document.html', '<html><head><title><\/title><link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,800" rel="stylesheet">' + ((hasHighlights) ? '<script src="https://unpkg.com/tippy.js@2.5.4/dist/tippy.all.min.js"><\/script>' : '') + '<style>.container { font-family: Nunito Sans,Avenir,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Ubuntu,Cantarell,Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 1.125rem; padding: 4%; margin: 0 auto; }' + ((hasHighlights) ? ' .tippy { background: rgba(255,202,223,.5); color: #d22489; outline: 0; border-bottom: 1px solid #f27da6; padding: 0 4px; font-weight: 600; cursor: help; }' : '') + '<\/style><script type="application/ld+json">' + JSON.stringify(schemaJson) + '<\/script><\/head><body class="container"' + ((hasHighlights) ? ' onload="tippy(\'.tippy\');"' : '') + '>' + text + '<\/body><\/html>');
+  download('document.html', '<html><head><title><\/title><link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,800" rel="stylesheet">' + ((hasHighlights) ? '<script src="https://unpkg.com/tippy.js@2.5.4/dist/tippy.all.min.js"><\/script>' : '') + '<style>.container { font-family: Nunito Sans,Avenir,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Ubuntu,Cantarell,Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 1.125rem; padding: 4%; margin: 0 auto; }' + ((hasHighlights) ? ' .tippy { background: rgba(255,202,223,.5); color: #d22489; outline: 0; border-bottom: 1px solid #f27da6; padding: 0 4px; font-weight: 600; cursor: help; }' : '') + '<\/style><script type="application/ld+json">' + JSON.stringify(schemaJson) + '<\/script><\/head><body class="container"' + ((hasHighlights) ? ' onload="tippy(\'.tippy\');"' : '') + '>' + (hasHighlights ? annText : text) + '<\/body><\/html>');
 }
 
 export {saveAsHTML, saveAsJSON};
